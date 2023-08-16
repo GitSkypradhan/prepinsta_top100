77422ee084d7048d9deb3608ef5e904dae87f5e6
GitSkypradhan <pradhanakash745@gmail.com>
2023-08-16 23:55:42 +0530

    ver 0.3

--- ASCII_table/find_prime_inrange.py
+++ ASCII_table/find_prime_inrange.py
@@ -1,3 +1,9 @@
-
+ 
 low_limit = int(input('Enter number to start: '))
 high_limit = int(input('Enter number to end: '))
+ 
+for i in range(low_limit,high_limit):
+        print(i)   
+        for j in range(i):
+                print(j,end=" ")
+        
