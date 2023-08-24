a1b1a47ab8170a3cc9caaa78adf7da770397c4fd
GitSkypradhan <pradhanakash745@gmail.com>
2023-08-25 01:02:40 +0530

    prime in range

--- ASCII_table/find_prime_inrange.py
+++ ASCII_table/find_prime_inrange.py
@@ -3,6 +3,24 @@ low_limit,high_limit = input('Enter number to (start,end): ').split(',')
 prime = []
 
 for num in range(int(low_limit),int(high_limit)+1):
+    if num < 0:
+        continue
+    if num == 2:
+        prime.append(num)
     for i in range(2,num):
-        print(num,i,end=' ')
-    print()
+        if num % i == 0:
+            break
+    else:
+        if num not in prime:
+            prime.append(num)
+
+print(prime)
+
+            
+
+            
+
+
+
+
+        
