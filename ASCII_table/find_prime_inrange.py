5990e03f391f0c40da3cf921ad2e9338a85ac1ce
GitSkypradhan <pradhanakash745@gmail.com>
2023-08-24 23:55:44 +0530

    ver 0.3

--- ASCII_table/find_prime_inrange.py
+++ ASCII_table/find_prime_inrange.py
@@ -2,6 +2,7 @@
 low_limit,high_limit = input('Enter number to (start,end): ').split(',')
 prime = []
 
-for i in range(int(low_limit),int(high_limit)):
-    for j in range(2,i):
-        print(j,end='')
+for num in range(int(low_limit),int(high_limit)+1):
+    for i in range(2,num):
+        print(num,i,end=' ')
+    print()
