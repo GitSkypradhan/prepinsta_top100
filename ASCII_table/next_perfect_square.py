f418780501568add5216cbc6b16b7aec26f9c97a
GitSkypradhan <pradhanakash745@gmail.com>
2023-08-26 15:29:14 +0530

    Find Next Perfect square

--- ASCII_table/next_perfect_square.py
+++ ASCII_table/next_perfect_square.py
@@ -1,6 +1,6 @@
 # A perfect square is an integer ,the square root of which is also an integer.
 import math
 def next_perfect_square(num):
-    return (math.sqrt(num) ** 2) if math.sqrt(num) % 1 == 0  else "Not a perfect square  itself." 
+    print(int(math.sqrt(num)+1)**2) if math.sqrt(num) % 1 == 0 else print("number itself is not perfect square.")   
 
 next_perfect_square(4)
