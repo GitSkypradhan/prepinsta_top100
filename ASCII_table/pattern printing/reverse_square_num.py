08ea862fe746d9991efc4b7fd7db65ee4a70a269
GitSkypradhan <pradhanakash745@gmail.com>
2023-09-07 23:02:53 +0530

    ver 0.3

--- ASCII_table/pattern printing/reverse_square_num.py
+++ ASCII_table/pattern printing/reverse_square_num.py
@@ -7,3 +7,14 @@ for i in range(10):
     for j in range(i,0,-1):
         print(j,end="")
     print()
+
+
+for i in range(10):
+    for j in range(10,0,-1):
+        print(chr(64+j),end='')
+    print()
+
+for i in range(10):
+    for j in range(10,0,-1):
+        print(j,end=' ')
+    print()
