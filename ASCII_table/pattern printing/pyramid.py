a40d913cd0d3cfdc36bcbaf10ef186a30390393b
GitSkypradhan <pradhanakash745@gmail.com>
2023-09-11 23:28:00 +0530

    ver0.3

--- ASCII_table/pattern printing/pyramid.py
+++ ASCII_table/pattern printing/pyramid.py
@@ -10,7 +10,25 @@ def num_pyramid(num):
             print(j,end='')
         print()
   
+def reverse_pyramid(num):
+    # for i in range(num,0,-1):
+    #     for j in range(1,i+1):
+    #         print(j,end='')
+    #     print()
+    for i in range(1,num+1):
+        for j in range(1,num+2-i):
+            print(j,end='')
+        print()
+
+def triangle_pyramid(num):
+    for i in range(1,num+1):
+        print("o"*(num+i),end='')  
+        print()
+
 
 n = int(input('Enter a number: '))
 # num_pyramid(n) 
-star_pyramid(n)
+# star_pyramid(n)
+# reverse_pyramid(n)
+
+triangle_pyramid(n)
