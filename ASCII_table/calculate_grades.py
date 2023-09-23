4cea1554191efbfecfc685b5b67380644b1670ed
GitSkypradhan <pradhanakash745@gmail.com>
2023-09-23 23:09:58 +0530

    ver 0.3

---
+++ ASCII_table/calculate_grades.py
@@ -0,0 +1,21 @@
+print("Enter your marks: ")
+sub1 = int(input('First Subject: '))
+sub2 = int(input('Second Subject: '))
+sub3 = int(input('Third Subject: '))
+sub4 = int(input('Fourth Subject: '))
+sub5 = int(input('Fifth Subject: '))
+
+avg = (sub1+sub2+sub3+sub4+sub5)/5
+
+if avg >= 90:
+    print('Grade: A')
+elif avg >= 80:
+    print('Grade B')
+elif avg >= 70:
+    print('Grade C')
+elif avg >= 60:
+    print('Grade D')
+else:
+    print('Grade: F')
+
+    
